dc4b2ed593714013743dee2a2cee89a86c15d624
Maxim <blekbjek@gmail.com>
2023-04-14 11:08:20 +0300

    Add verifying JWT Tokens function and fix some bugs

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,11 +1,15 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security.oauth2 import OAuth2AuthorizationCodeBearer
 
 # Config of JWT token function 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = "a87c310a11366deac2cddad9559bd8f88b7883eed7be02ab816690530de100d9"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+oauth2_schema = OAuth2AuthorizationCodeBearer(tokenUrl='login')
 
 # Creating JWT token function
 def create_access_token(data: dict):
@@ -13,9 +17,41 @@ def create_access_token(data: dict):
     to_encode = data.copy()
 
     # Expire date of token
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({'exp': expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+
+# Veryfying JWT tokens
+def verify_access_token(token: str, login_exception):
+
+    try:
+        # Decoding JWT token and getting user id
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+
+        # If id doesn't exists -> raise exception
+        if id is None:
+            raise login_exception
+        
+        # Store fata from JWT token
+        token_data = schemas.TokenData(id=id)
+
+    except JWTError:
+        raise login_exception
+    
+    return token_data
+
+
+# Getting user id from JWT token
+def get_current_user(token: str = Depends(oauth2_schema)):
+
+    login_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                    detail='Could not validate login',
+                                    headers={'WWW-Authenticate': "Bearer"})
+
+    return verify_access_token(token, login_exception)
