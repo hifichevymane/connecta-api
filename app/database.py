d6556ea095470e2580494ae8985164bcfc331766
Maxim <blekbjek@gmail.com>
2023-04-17 11:46:48 +0300

    Replace SQLITE db to PostgreSQL. Hide all secret information in .env file

--- app/database.py
+++ app/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
 # Database url
-SQLALCHEMY_DATABASE_URL = 'sqlite:///business_cards.db'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Making a session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
