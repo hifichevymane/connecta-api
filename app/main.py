c5425bf14b8cbf5b5ecf6bb97d4dc0480558f616
Maxim <blekbjek@gmail.com>
2023-04-26 15:31:58 +0300

    Add CORS support

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import business_cards, users, auth
@@ -8,6 +9,15 @@ from .routers import business_cards, users, auth
 
 # Create an object FastAPI
 app = FastAPI()
+
+# Add CORS support
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 # Include all paths(routers)
 app.include_router(business_cards.router)
